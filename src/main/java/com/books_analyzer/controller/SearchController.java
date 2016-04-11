214992ef13ef37b93e89b35683705c02d91c8340
Nefisco <miguelasedilesdq@gmail.com>
2016-04-11 17:20:36 +0200

    Signed-off-by: Nefisco <miguelasedilesdq@gmail.com>

--- src/main/java/com/books_analyzer/controller/SearchController.java
+++ src/main/java/com/books_analyzer/controller/SearchController.java
@@ -1,12 +1,13 @@
 package com.books_analyzer.controller;
 
+import com.sun.org.apache.xpath.internal.operations.String;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.books_analyzer.service.BooksProcessor;
-
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SearchController {
@@ -22,10 +23,19 @@ public class SearchController {
 
 		System.out.println("SeachController: The parameters received are-");
 		//System.out.println("SeachController: " + String.join(",", title, author, character, url));
-		System.out.println("SeachController: " + String.join(",", title, author, character));
+		//System.out.println("SeachController: " + String.join(",", title, author, character));
     	/*booksProcessor = new BooksProcessor(title, author, character, url);
     	booksProcessor.process();
     	return booksProcessor.getJSON();*/
-		return booksProcessor.search(title,author,character,url);
+/*
+		Map <String, String> vars = new HashMap<String, String>();
+		vars.put("title",title);
+		vars.put("author",author);
+		vars.put("character",character);
+		vars.put("url",url);*/
+RestTemplate restTemplate = new RestTemplate();
+		//		restTemplate.getForObject("http://example.com/hotels/{hotel}/bookings/{booking}", String.class,title,author,character,url);
+		//return restTemplate.getForObject("https://book-analyzer.herokuapp.com/", String.class,title,author,character,url);
+		return restTemplate.getForObject("https://book-analyzer.herokuapp.com/search/", String.class,title,author,character,url);
 	}
 }
